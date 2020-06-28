db8ee8e5e175a245038870f3308a9003514ec6f0
Javier Montero <monterodotjavier@icloud.com>
2020-06-27 21:59:21 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Translate Instant Messenger
+
+Messenger application that allows user to message between eachother.
+
+- Real-time (Firebase)
+- Translation
+- Notifications
+- Audio
+- Add typing notification (someone is typing…)
+
+Show some pictures!!!!!
+
+## Usage
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install foobar.
+
+- Socket
+- Express
+- Material UI
+- React
+- and more
+
+## Template place holder
+
+```python
+import foobar
+
+foobar.pluralize('word') # returns 'words'
+foobar.pluralize('goose') # returns 'geese'
+foobar.singularize('phenomena') # returns 'phenomenon'
+```
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ server/index.js
@@ -0,0 +1,118 @@
+const path = require("path");
+const http = require("http");
+const express = require("express");
+
+const socketio = require("socket.io");
+const app = express();
+const server = http.createServer(app);
+const PORT = process.env.PORT || 8080;
+const io = socketio(server);
+
+const botName = "TIM";
+
+const formatMessage = require("./utils/messages");
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require("./utils/users");
+
+const createApp = () => {
+  // static file-serving middleware
+  app.use(express.static(path.join(__dirname, "..", "/public/web")));
+
+  // any remaining requests with an extension (.js, .css, etc.) send 404
+  app.use((req, res, next) => {
+    if (path.extname(req.path).length) {
+      const err = new Error("Not found");
+      err.status = 404;
+      next(err);
+    } else {
+      next();
+    }
+  });
+
+  // sends index.html
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "public/web"));
+  });
+
+  // Connecting socket. HTTP server that will take care of starting the server and handling interactions with Socket.io ( sending and recieving messages)
+  io.on("connection", (socket) => {
+    socket.on("joinRoom", ({ username, room }) => {
+      const user = userJoin(socket.id, username, room);
+      socket.join(user.room);
+
+      // Welcome current user
+      socket.emit("message", formatMessage(botName, "Welcome to TIM!"));
+
+      // Broadcast when a user connects
+      socket.broadcast
+        .to(user.room)
+        .emit(
+          "message",
+          formatMessage(botName, `${user.username} has joined the chat`)
+        );
+
+      // Send users and room info
+      io.to(user.room).emit("roomUsers", {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      });
+    });
+
+    // Listen for chatMessage
+    socket.on("chatMessage", (msg) => {
+      const user = getCurrentUser(socket.id);
+
+      io.to(user.room).emit("message", formatMessage(user.username, msg));
+    });
+
+    // Runs when client disconnects
+    socket.on("disconnect", () => {
+      const user = userLeave(socket.id);
+
+      if (user) {
+        io.to(user.room).emit(
+          "message",
+          formatMessage(botName, `${user.username} has left the chat`)
+        );
+
+        // Send users and room info
+        io.to(user.room).emit("roomUsers", {
+          room: user.room,
+          users: getRoomUsers(user.room),
+        });
+      }
+    });
+  });
+  // error handling endware
+  app.use((err, req, res, next) => {
+    console.error(err);
+    console.error(err.stack);
+    res.status(err.status || 500).send(err.message || "Internal server error.");
+  });
+};
+
+const startListening = () => {
+  // start listening (and create a 'server' object representing our server)
+  server.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`));
+  // set up our socket control center
+};
+
+async function bootApp() {
+  await createApp();
+  await startListening();
+}
+// This evaluates as true when this file is run directly from the command line,
+// i.e. when we say 'node server/index.js' (or 'nodemon server/index.js', or 'nodemon server', etc)
+// It will evaluate false when this module is required by another module - for example,
+// if we wanted to require our app in a test spec
+if (require.main === module) {
+  bootApp();
+} else {
+  createApp();
+}
+
+module.exports = app;
